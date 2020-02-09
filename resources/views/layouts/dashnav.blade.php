fafbb4f5265ff99c4486cf17866b97b803e58d97
Igor Stojanovski <cableclark@gmail.com>
2020-02-09 19:19:20 +0100

    finish login area styling

--- resources/views/layouts/dashnav.blade.php
+++ resources/views/layouts/dashnav.blade.php
@@ -1,19 +1,22 @@
 @auth
+<nav>
+    <button class="dashboard--button">Menu</button>
+    <div class="dashboard--navbar">
+        <h3>Администраторски панел</h3> 
+        <ul>
+            <a href="{{route('admin')}}"> <li>Постови </li></a>
+            <a href="{{route('show-images')}}"> <li>Слики</li></a>
+            <a href="{{ route('logout') }}"
+                onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                <li> Одлогирај се</li>
+            </a>
+            <form  id='logout-form' action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>~
+        </ul>            
 
-<nav class="dashboard--navbar">
-    <h3>Администраторски панел</h3> 
-    <ul>
-        <a href="{{route('admin')}}"> <li>Постови </li></a>
-        <a href="{{route('show-images')}}"> <li>Слики</li></a>
-        <a href="{{ route('logout') }}"
-            onclick="event.preventDefault();
-                            document.getElementById('logout-form').submit();">
-            <li> Одлогирај се</li>
-        </a>
-        <form  id='logout-form' action="{{ route('logout') }}" method="POST" style="display: none;">
-            @csrf
-        </form>
-    </ul>            
+    </div>
 
 </nav>
 
